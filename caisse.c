dc6159f6f67330c2b201f0cc3566e8a6bfd48a68
Neel131attempt <nethaneel131@gmail>
2025-04-17 00:01:54 +0400

    v0.3 le code bug acause d'une }

--- caisse.c
+++ caisse.c
@@ -46,23 +46,65 @@ printf("Monnaie a rendre : Rs%.2lf \n", Monnaie);
 
 double Billets[7] = {2000, 1000, 500, 200, 100, 50, 25};
 int BilletsUtilise[7] = {0};
+int BilletsRestant[7] = {2,2,2,2,4,4,8};
 
 double Pieces[7] = {20.00,10.00,5.00,1.00,0.50,0.20,0.05};
 int PiecesUtilise[7] = {0};
+int PiecesRestant[7] = {2,2,2,2,4,10};
+int CaisseVide = 1;
+int choix = 0;
+
+//Affichage
+for (int x = 0; x < 7; ++x){
+printf("Rs%.2lf : %d \n", Billets[x], BilletsRestant[x]);
+printf("Rs%.2lf : %d \n", Pieces[x], PiecesRestant[x]);
+}
+
+
+//calculation
+
+
+while (CaisseVide == 1){
 
 for (int x; x < 7 ; ++x){
-	while (Monnaie >= Billets[x]) {
-			Monnaie = Monnaie - Billets[x];
-			BilletsUtilise[x] = BilletsUtilise[x] + 1; 
+	while (BilletsRestant[x] > 0){
+		while (Monnaie >= Billets[x]) {
+				Monnaie = Monnaie - Billets[x];
+				BilletsUtilise[x] = BilletsUtilise[x] + 1; 
+				BilletsRestant[x] = BilletsRestant[x] - 1;
+				printf("on a utiliser : Rs%.2lf et il reste %d Billets \n", Billets[x], BilletsRestant[x]);
+		}
 	}
 }
 
 for (int x; x < 7 ; ++x){
-	while (Monnaie >= Pieces[x] - 0.00000000001) {
-			Monnaie = Monnaie - Pieces[x];
-			PiecesUtilise[x] = PiecesUtilise[x] + 1;
+	while (PiecesRestant[x] > 0){
+		while (Monnaie >= Pieces[x] - 0.00000000001) {
+				Monnaie = Monnaie - Pieces[x];
+				PiecesUtilise[x] = PiecesUtilise[x] + 1;
+				PiecesRestant[x] = PiecesRestant[x] - 1;
+				printf("on a utiliser : Rs%.2lf et il reste %d piecess \n", Pieces[x], PiecesRestant[x]);
+		}
 	}
 }
+if (Monnaie >= 0.05){
+	printf("voulez-vous remplir la caisse encore (tapez 1) ou Arreter maintenant (tapez 2)?\n ");
+	scanf("%d", &choix);
+
+	if (choix == 1){
+for (int x = 0 ; x < 7 ; ++x) {
+	BilletsRestant[x] = 4 ;
+	PiecesRestant[x] = 4;
+}
+}
+else if (choix == 2) {
+	CaisseVide = 0;
+}
+else{
+	printf("vous devez mettre entre 1 et 2 uniquement !!!");
+}
+}
+//Affichage
 printf("vous avez recu en billets: \n");
 
 for (int x = 0; x < 7; ++x){
