d2983c449d8b07f823e9f6ceaea8ece118d327e5
Neel131attempt <nethaneel131@gmail>
2025-04-16 22:53:06 +0400

    Rendu de la monnaie avec la quantite de billets et pieces utilise

--- caisse.c
+++ caisse.c
@@ -4,7 +4,10 @@
 #include <math.h>
 #include <stdbool.h>
 
-
+double CalculeMonnaie(double Montant, double MontantDonne)
+{
+	return (MontantDonne - Montant);
+}
 
 int main ()
 {
@@ -12,21 +15,23 @@ int main ()
 srand(time(NULL)) ;
 
 double Montant  = 0.00 ;
-int flag = 1 ; // avec bool sa ne marcher pas alors j'ai oppter pour un petit 'pseudo-bool'
+int Flag = 1 ; // avec bool sa ne marcher pas alors j'ai oppter pour un petit 'pseudo-bool'
 double MontantDonne = 0.00;
-double echange = 0.00;
 int RandomNum = 0;
+double Monnaie = 0.00;
+
 RandomNum = (rand() % 200000) + 1; // j'ai mis un gros montant en prevision des prochaines etapes du code et aussi acause de l'inflation et au supermarcher ces prix sont realistique :(
 				   //
 Montant = RandomNum / 20.00 ; // en plus j'ai vue la convertion d'un int vers un double est plus facile que de generer un random double.
 
-while (flag == 1) {
+
+while (Flag == 1) {
 	printf("vous devez payer Rs%.2lf, Combien donnez vous ?\n", Montant);
 	scanf("%lf",&MontantDonne);
 
 if (MontantDonne > Montant)
 {
-	flag = 0;
+	Flag = 0;
 	printf("merci, nous calculons la monnaie sur ce montant \n");
 }else{
 	printf("vous n'avez pas donner assez. pouvez vous donner plus ?");
@@ -34,6 +39,42 @@ if (MontantDonne > Montant)
 
 }
 
-return 0;
-	
+Monnaie = CalculeMonnaie(Montant, MontantDonne);
+printf("Monnaie a rendre : Rs%.2lf \n", Monnaie);
+
+// section pour les billets et les pieces 
+
+double Billets[7] = {2000, 1000, 500, 200, 100, 50, 25};
+int BilletsUtilise[7] = {0};
+
+double Pieces[7] = {20.00,10.00,5.00,1.00,0.50,0.20,0.05};
+int PiecesUtilise[7] = {0};
+
+for (int x; x < 7 ; ++x){
+	while (Monnaie >= Billets[x]) {
+			Monnaie = Monnaie - Billets[x];
+			BilletsUtilise[x] = BilletsUtilise[x] + 1; 
+	}
+}
+
+for (int x; x < 7 ; ++x){
+	while (Monnaie >= Pieces[x] - 0.00000000001) {
+			Monnaie = Monnaie - Pieces[x];
+			PiecesUtilise[x] = PiecesUtilise[x] + 1;
+	}
+}
+printf("vous avez recu en billets: \n");
+
+for (int x = 0; x < 7; ++x){
+		printf("Rs%.2lf : %d \n", Billets[x], BilletsUtilise[x]);
+}
+
+printf("vous avez recu en pieces: \n");
+for (int x = 0; x < 7; ++x){
+		printf("Rs%.2lf : %d \n", Pieces[x], PiecesUtilise[x]);
+}
+
+	return 0;
+
+
 }
